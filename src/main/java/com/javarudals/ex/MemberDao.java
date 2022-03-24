cd3b62b582c0c099c45cf791fd2126980a18b7d0
경민 <rodrng@gmail.com>
2022-03-24 12:50:40 +0900

    아이디 중복 체크 추가

--- src/main/java/com/javarudals/ex/MemberDao.java
+++ src/main/java/com/javarudals/ex/MemberDao.java
@@ -3,13 +3,21 @@ package com.javarudals.ex;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MemberDao {
 	
-	private static MemberDao instance = new MemberDao(); // 싱글턴 패턴
 	
-	private MemberDao() {
+	static String driverName = "com.mysql.jdbc.Driver";
+	static String url = "jdbc:mysql://localhost:3306/memberdb";
+	static String user = "root";
+	static String password = "123qwe";
+	
+	
+	private static MemberDao instance = new MemberDao();//싱글턴 패턴
+	
+	private MemberDao() {		
 		
 	}
 	
@@ -17,9 +25,8 @@ public class MemberDao {
 		
 		return instance;
 	}
-	
-	
-	public int insertMember(MemberDto dto) {
+
+	public int insertMember(MemberDto dto) {		
 		
 		String m_id = dto.getId();
 		String m_pw = dto.getPw();
@@ -28,47 +35,73 @@ public class MemberDao {
 		String m_address = dto.getAddress();
 		
 		int flag = 0;
-		String driverName = "com.mysql.jdbc.Driver";
-	    String url = "jdbc:mysql://localhost:3306/memberdb";
-	    String user = "root";
-	    String password = "123qwe";
+		
 		
 		Connection connection = null;
 		PreparedStatement pstmt = null;
-		String query = "insert into members values ('"+m_id+"', '"+m_pw+"', '"+m_name+"', '"+m_eMail+"','"+m_address+"')";
-//		String query = "insert into members values (?, ?, ?, ?, ?)";
+		String query = "insert into members(id, pw, name, eMail, address) values('"+ m_id + "','"+ m_pw +"','" + m_name + "','" + m_eMail + "','"+ m_address + "')";
+//		String query = "insert into members(id, pw, name, eMail, address) values(?, ?, ?, ?, ?)";
 		
-		 try{
-		       Class.forName(driverName);
-		       connection = DriverManager.getConnection(url, user, password);
-		       pstmt = connection.prepareStatement(query); // sql을 실행시켜주는 객체 생성(Statement)	       
-//		       pstmt.setString(1, m_id);
-//		       pstmt.setString(2, m_pw);
-//		       pstmt.setString(3, m_name);
-//		       pstmt.setString(4, m_eMail);
-//		       pstmt.setString(5, m_address);
-		       
-		       flag = pstmt.executeUpdate(query); // sql문이 성공적으로 실행되면 1을 반환
-		       
-		       
-		    } catch(Exception e) {
-		       e.printStackTrace();
-		    
-		    } finally {
-		       try{
-		    	   if(pstmt != null){
-		    		   pstmt.close();
-			          }
-		          if(connection != null){
-		        	  connection.close();
-		          }
-		       } catch(Exception e2) {
-		          e2.printStackTrace();
-		       }
-		       
-		    }
-		 
+		
+		try{
+			Class.forName(driverName);
+			connection = DriverManager.getConnection(url, user, password);
+			pstmt = connection.prepareStatement(query);//sql을 실행시켜주는 객체 생성(Statement)
+//			pstmt.setString(1, m_id);
+//			pstmt.setString(2, m_pw);
+//			pstmt.setString(3, m_name);
+//			pstmt.setString(4, m_eMail);
+//			pstmt.setString(5, m_address);			
+			
+			flag = pstmt.executeUpdate(query);//sql문이 성공적으로 실행되면 flag=1을 반환
+			
+		} catch(Exception e) {
+			e.printStackTrace();		
+		} finally {
+			try{
+				if(pstmt != null){					
+					pstmt.close();
+				}
+				if(connection != null){					
+					connection.close();
+				}
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
+			
+		}
+		
+		return flag;		
+	}
+	
+	
+	public int confirmId(String id) {
+		int flag = 0;
+		
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet set = null;
+		String query = "select id from members where id = ?";
+		
+		try {
+			Class.forName(driverName);
+			connection = DriverManager.getConnection(url, user, password);
+			pstmt = connection.prepareStatement(query);//sql을 실행시켜주는 객체 생성(Statement)
+			pstmt.setString(1, id);
+			set = pstmt.executeQuery();
+			
+			if(set.next()) {//조건이 참이면 DB에 이미 똑같은 아이디 있음
+				flag = 1;
+			} else {
+				flag = 0;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return flag;
 	}
+		
+	
 }
--- src/main/webapp/joinOk.jsp
+++ src/main/webapp/joinOk.jsp
@@ -1,28 +1,39 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<% request.setCharacterEncoding ("UTF-8"); %>
-<%@ page import ="com.javarudals.ex.*" %>
+<% request.setCharacterEncoding("UTF-8"); %>
+<%@ page import = "com.javarudals.ex.*" %>
 <jsp:useBean id="dto" class="com.javarudals.ex.MemberDto"></jsp:useBean>
 
 <jsp:setProperty property="*" name="dto"/>
 <%
 	MemberDao dao = MemberDao.getInstance();
 
-	int ret = dao.insertMember(dto); // 1이 반환되면 db 저장 성공 
-	if (ret == 1){
+	if(dao.confirmId(dto.getId()) == 1) {
 %>
 	<script type="text/javascript">
-		alert("회원가입성공!가입을 축하합니다.");
+		alert("이미 존재하는 아이디입니다. 다시 입력해주세요.");
+		history.back();//뒤로가기
+	</script>
+		
+<%	} else {
+	
+	int ret = dao.insertMember(dto);//1이 반환되면 db 저장 성공
+	if (ret == 1) {
+%>
+	<script type="text/javascript">
+		alert("회원가입성공!가입을 축하합니다.");		
 	</script>
 <%
-	}else{
+	} else {
 %>
 	<script type="text/javascript">
-		alert("회원가입실패!다시 확인해주세요.");
+		alert("회원가입실패!다시 확인해주세요.");		
 	</script>
-<%}
-
+<% 
+	}
+}
 %>
+
 
 <!DOCTYPE html>
 <html>
